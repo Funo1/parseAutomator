4e7cfaafa9dcc57be5bf407a6ba50ca1c68067c0
funo1 <cajdorsett@gmail.com>
2024-03-03 00:16:34 -0500

    Merge changes from remote repository

--- ParseAutomator.py
+++ ParseAutomator.py
@@ -1,9 +1,12 @@
 import os
 import time
 import re
+
 Sequence = []
 parsedChar = []
 incrs = []
+
+OrderOfEvents = []
 
 
 # Looks for sequences
@@ -40,17 +43,21 @@ def numerate(output):
     for i in range(len(output)):
         if output[i] != output[i - 1]:
             Sequence.append(output[i])
-    formatMacro(Sequence)
+    return formatMacro(Sequence)
+
 
 # Code format UwU:
 def formatMacro(Sequence):
+    formatted_commands = []
     for x in range(len(Sequence)):
-        print(
-f'''for i in range({incrs[x]}):
+            cmdLoop = f'''
+for i in range({incrs[x]}):
     pyautogui.keyDown('{Sequence[x]}')
 pyautogui.keyUp('{Sequence[x]}')
-        '''
-        )
+'''
+    formatted_commands.append(cmdLoop)
+    return formatted_commands
+
 
 # Parses macro into text
 def parseAutomator(automator_script):
@@ -76,8 +83,9 @@ def parseAutomator(automator_script):
                 pyautogui_commands.append(f"time.sleep({delay})")
             except ValueError:
                 print("Invalid delay value:", line)
-    
+
     return pyautogui_commands
+
 
 automator_script_file = 'src/inputs.txt'
 
@@ -85,23 +93,32 @@ with open(automator_script_file, 'r') as file:
     automator_script = file.read()
 
 pyautogui_commands = parseAutomator(automator_script)
+formatted_commands_list = []
+
 if pyautogui_commands:
     # Print the translated PyAutoGUI commands
     for command in pyautogui_commands:
         if len(command.split('⌃')) > 1:
             output = f"pyautogui.hotkey('ctrl','{command.split('⌃')[1]}"
-            print(output)
-        else: 
+            formatted_commands_list.append(output)
+        else:
             if command.startswith('time.sleep'):
+<<<<<<< HEAD
                 print(f"{command} \n")
+=======
+                formatted_commands_list.append(command)
+>>>>>>> 6ef75c6 (Merge changes from remote repository)
             else:
-                numerate(command)
+                formatted_commands_list.extend(numerate(command))
+        OrderOfEvents.append(command)
+
+def sort(formatted_commands_list):
+    for k in range(len(formatted_commands_list)):
+        if formatted_commands_list[k].startswith('time'):
+            formatted_commands_list[k], formatted_commands_list[k-1] = formatted_commands_list[k - 1], formatted_commands_list[k]
+    for l in range(len(formatted_commands_list)):
+        print(formatted_commands_list[l])
+sort(formatted_commands_list)
 
 
-
-
-
-
-             
-    
 
